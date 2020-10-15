9fa50b0e62f8efb9229953aa1f7084c1b0377dbe
Henning Becker <henning.becker@gmail.com>
2020-10-15 08:56:01 +0200

    Restrict Disassembly Action to one function
    Previously it was possible to select multiple functions and click "Go to
    Disassembly" and Orbit would open as many Disassembly windows as needed.
    
    This workflow is not (yet) supported by the web-based Disassembly View
    and we also want to avoid that the user has to juggle with multiple
    dialog windows. This will be solved differently in the future, probably
    with something like tabs inside a single window.

--- OrbitGl/SamplingReportDataView.cpp
+++ OrbitGl/SamplingReportDataView.cpp
@@ -187,7 +187,7 @@ std::vector<std::string> SamplingReportDataView::GetContextMenu(
   absl::flat_hash_set<const FunctionInfo*> selected_functions =
       GetFunctionsFromIndices(selected_indices);
 
-  bool enable_disassembly = !selected_functions.empty();
+  const bool enable_disassembly = selected_functions.size() == 1;
 
   for (const FunctionInfo* function : selected_functions) {
     enable_select |= !GOrbitApp->IsFunctionSelected(*function);
